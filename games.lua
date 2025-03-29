115f10664613760478a1b333aa4843341b87a3b0
public-account-7 <doitenroimain@gmail.com>
2025-03-30 03:19:59 +0700

    Create games.lua

---
+++ games.lua
@@ -0,0 +1,19 @@
+local Games = {
+  [2753915549] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/Blox%20Fruit.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/Blox%20Fruit.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/Blox%20Fruit.lua", -- Blox Fruit Sea 3
+  [3623096087] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/Muscle%20Legends.lua", -- Muscle Legends
+  [8737899170] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/PS99.lua",  -- PS99 World 1
+  [16498369169] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/PS99.lua",  -- PS99 World 2
+  [17503543197] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/PS99.lua",  -- PS99 World 3
+  [10449761463] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/The%20Strongest%20Battleground.lua",  -- The Strongest Battlegrounds
+  [18901165922] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/Pet%20GO.lua", -- Pet GO
+  [16732694052] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/Fisch.lua", -- Fisch
+  [72907489978215] = "https://raw.githubusercontent.com/public-account-7/grrrrrrrr/refs/heads/main/Fisch.lua", -- 2 Sea Fisch
+  [116495829188952] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+  [70876832253163] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Dead%20Rails.lua", -- Dead Rails
+  [125723653259639] = "https://raw.githubusercontent.com/triquqd717/main/refs/heads/main/other/DrillDiggingSimulator.lua", -- Drill Digging Simulator
+  [87039211657390] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Arise%20Crossover.lua", -- Arise Crossover
+}
+
+return Games
